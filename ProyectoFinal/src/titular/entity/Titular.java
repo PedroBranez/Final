0796fbd6d4afadb31236a6086b98feff02e91539
Your Name <you@example.com>
2018-09-10 13:03:52 -0400

    Subiendo Titular

---
+++ ProyectoFinal/src/titular/entity/Titular.java
@@ -0,0 +1,52 @@
+package titular.entity;
+
+public class Titular {
+	private int CI;
+	private String nombre;
+	private int teléfono;
+	private String dirección;
+	private int NIT;
+	public Titular(int cI, String nombre, int teléfono, String dirección, int nIT) {
+		super();
+		CI = cI;
+		this.nombre = nombre;
+		this.teléfono = teléfono;
+		this.dirección = dirección;
+		NIT = nIT;
+	}
+	public int getCI() {
+		return CI;
+	}
+	public void setCI(int cI) {
+		CI = cI;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getTeléfono() {
+		return teléfono;
+	}
+	public void setTeléfono(int teléfono) {
+		this.teléfono = teléfono;
+	}
+	public String getDirección() {
+		return dirección;
+	}
+	public void setDirección(String dirección) {
+		this.dirección = dirección;
+	}
+	public int getNIT() {
+		return NIT;
+	}
+	public void setNIT(int nIT) {
+		NIT = nIT;
+	}
+	@Override
+	public String toString() {
+		return "Titular [CI=" + CI + ", nombre=" + nombre + ", teléfono=" + teléfono + ", dirección=" + dirección
+				+ ", NIT=" + NIT + "]";
+	}
+}
