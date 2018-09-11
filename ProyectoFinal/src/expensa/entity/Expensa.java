96221222a625b5de20c88e04e2e95a84291fe7a0
Rodrigobarrero <rbarreo10@gmail.com>
2018-09-11 13:53:04 -0400

    Modificando Expensa

--- ProyectoFinal/src/expensa/entity/Expensa.java
+++ ProyectoFinal/src/expensa/entity/Expensa.java
@@ -1,5 +1,45 @@
 package expensa.entity;
 
 public class Expensa {
-
+	private int nroOficina;
+	private double costoTeléfono;
+	private double costoLuz;
+	private double costoAgua;
+	public Expensa(int nroOficina, double costoTelefono, double costoLuz, double costoAgua) {
+		super();
+		this.nroOficina = nroOficina;
+		this.costoTeléfono = costoTelefono;
+		this.costoLuz = costoLuz;
+		this.costoAgua = costoAgua;
+	}
+	public int getNroOficina() {
+		return nroOficina;
+	}
+	public void setNroOficina(int nroOficina) {
+		this.nroOficina = nroOficina;
+	}
+	public double getCostoTeléfono() {
+		return costoTeléfono;
+	}
+	public void setCostoTeléfono(double costoTelefono) {
+		this.costoTeléfono = costoTelefono;
+	}
+	public double getCostoLuz() {
+		return costoLuz;
+	}
+	public void setCostoLuz(double costoLuz) {
+		this.costoLuz = costoLuz;
+	}
+	public double getCostoAgua() {
+		return costoAgua;
+	}
+	public void setCostoAgua(double costoAgua) {
+		this.costoAgua = costoAgua;
+	}
+	@Override
+	public String toString() {
+		return "Expensa [nroOficina=" + nroOficina + ", costoTelefono=" + costoTeléfono + ", costoLuz=" + costoLuz
+				+ ", costoAgua=" + costoAgua + "]";
+	}
+	
 }
--- ProyectoFinal/src/expensa/view/ExpensaIO.java
+++ ProyectoFinal/src/expensa/view/ExpensaIO.java
@@ -1,6 +1,19 @@
 package expensa.view;
 
+import java.util.Scanner;
+
+import expensa.entity.Expensa;
+import view.InputTypes;
+
 public class ExpensaIO {
-	
+	public static Expensa ingresar (Scanner scanner){
+		int nroOficina = InputTypes.readInt("Ingrese el número de Oficina", scanner);
+		double costoTeléfono = InputTypes.readDouble("Ingrese el costo de telefono", scanner);
+		double costoLuz = InputTypes.readDouble("Ingrese el costo de Luz", scanner);
+		double costoAgua = InputTypes.readDouble("Ingrese el costo de Agua", scanner);
+		
+		return new Expensa(nroOficina, costoTeléfono,costoLuz,costoAgua);
+
+	}
 
 }
